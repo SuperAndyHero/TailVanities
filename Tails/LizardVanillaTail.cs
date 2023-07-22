bb191c52fdcccb3891e1e5a3e198f1485b5f8467
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2023-07-22 06:16:02 -0500

    lizard tail fixed

--- Tails/LizardVanillaTail.cs
+++ Tails/LizardVanillaTail.cs
@@ -11,7 +11,7 @@ namespace TailVanities.Tails
     public class LizardVanillaTail : Tailbase
     {
         public override string Texture => "TailVanities/Tails/LizardVanilla";
-        public override Vector2 WorldOffset => new Vector2(9.6f, 11.4f);
+        public override Vector2 WorldOffset => new Vector2(2f, 11.2f);
         public override bool PreUpdate()
         {
             //tailInstance.TailWobble(3, 4, 0.03f, 0.14f, 0.35f, 0.55f);//wobbles the tail when idle or running
@@ -19,28 +19,31 @@ namespace TailVanities.Tails
         }
 
         public override float Width => 6f;
-        public override Vector2 TexPosOffset => new Vector2(-7.450581f, -6.850011f);
-        public override Vector2 TexSizeOffset => new Vector2(1.3000002f, 6.8999977f);
+        public override Vector2 TexPosOffset => new Vector2(-0.049999993f, -6.5000067f);
+        public override Vector2 TexSizeOffset => new Vector2(0.5500001f, 6.5f);
         public override int PhysicsRepetitions => 5;
         public override float VertexDrag => 1.15f;
-        public override int VertexCount => 4;
+        public override int VertexCount => 5;
         public override float[] VertexDistances => new float[] {
-            5.82f,
-            5.90028f,
-            7.82039f,
-            7.5857787f,0f
+            6.2361274f,
+            4.7728934f,
+            4.5107613f,
+            4.164102f,
+            4.0391793f
         };
         public override Vector2[] VertexGravityForces => new Vector2[] {
             new Vector2(0f, 0f),
-            new Vector2(0.30955127f, -0.3757273f),
-            new Vector2(0.9791489f, -0.20314376f),
-            new Vector2(0.858299f, 0.5131497f)
+            new Vector2(1.1716537f, -0.2223282f),
+            new Vector2(0.66171914f, -0.22827977f),
+            new Vector2(0.65638757f, 0.24321863f),
+            new Vector2(0.6375457f, 0.28902507f)
         };
         public override Vector2[] SettledPoints => new Vector2[] {
             new Vector2(0f, 0f),
-            new Vector2(5.9875793f, 0.0075821355f),
-            new Vector2(11.875526f, 1.0292786f),
-            new Vector2(18.721209f, 4.8101697f)
+            new Vector2(6.4927273f, 0.33783394f),
+            new Vector2(11.377643f, 1.17394f),
+            new Vector2(15.688381f, 2.7898016f),
+            new Vector2(19.492243f, 4.484022f)
         };
 
     }
